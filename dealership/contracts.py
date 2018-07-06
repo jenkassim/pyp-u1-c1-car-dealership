572c6e4aa0499aa8afda885c14040010719751a5
jenkassim <jenkassim@gmail.com>
2018-07-07 01:37:05 +0800

    Dealership solution

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,41 @@
 class Contract(object):
-    pass
+    def __init__(self, vehicle, customer):
+        self.vehicle = vehicle
+        self.customer = customer
+
+    def total_value(self):
+        disc = 0
+        if self.customer.is_employee():
+            disc = 0.10 * self._total_value()
+
+        return self._total_value() - disc
+
+    def monthly_value(self):
+        return self.total_value() / self._monthly_value()
 
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
+        super(BuyContract, self).__init__(vehicle,customer)
+        self.monthly_payments = monthly_payments
+
+    def _total_value(self):
+        sale_price = self.vehicle.sale_price()
+
+        return sale_price + (self.vehicle.intr_rate * self.monthly_payments * sale_price / 100)
+
+    def _monthly_value(self):
+        return self.monthly_payments
 
 
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        super(LeaseContract, self).__init__(vehicle, customer)
+        self.length_in_months = length_in_months
+
+    def _total_value(self):
+        sale_price = self.vehicle.sale_price()
+        return sale_price + (sale_price * self.vehicle.lease_mul / self.length_in_months)
+
+    def _monthly_value(self):
+        return self.length_in_months
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,31 @@
 class Vehicle(object):
     def __init__(self, maker, model, year, base_price, miles):
-        pass
+        self.maker = maker
+        self.model = model
+        self.year = year
+        self.base_price = base_price
+        self.miles = miles
 
+    def sale_price(self):
+        return self.base_price * self.sale_multiplier
+
+    def purchase_price(self):
+        return self.sale_price() - (self.purc_multiplier * self.miles)
 
 class Car(Vehicle):
-    pass
-
+    sale_multiplier = 1.2
+    purc_multiplier = 0.004
+    intr_rate = 1.07
+    lease_mul = 1.2
 
 class Motorcycle(Vehicle):
-    pass
-
+    sale_multiplier = 1.1
+    purc_multiplier = 0.009
+    intr_rate = 1.03
+    lease_mul = 1
 
 class Truck(Vehicle):
-    pass
+    sale_multiplier = 1.6
+    purc_multiplier = 0.02
+    intr_rate = 1.11
+    lease_mul = 1.7
